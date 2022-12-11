2a25f7f2b5db98bc19bff03656b94f40309a2027
kamil.klimaszewski@gmail.com <kamil.klimaszewski@gmail.com>
2022-12-11 19:12:19 +0100

    init from start.spring.io

---
+++ src/test/java/pl/kamil/spring2/springbootstarter/SpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kamil.spring2.springbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
